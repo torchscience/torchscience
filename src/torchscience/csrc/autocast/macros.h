273b338fb4d632a69908a2b3da1451b18430175c
Allen Goodman <allen.goodman@icloud.com>
2025-12-12 13:36:51 -0500

    Add ternary, quaternary, and quinary Autocast macros

--- src/torchscience/csrc/autocast/macros.h
+++ src/torchscience/csrc/autocast/macros.h
@@ -70,3 +70,153 @@
       &SCHEMA_NAME                                                              \
     );                                                                          \
   }
+
+#define TORCHSCIENCE_TERNARY_AUTOCAST(SCHEMA_NAME, ARG0, ARG1, ARG2)            \
+  at::Tensor SCHEMA_NAME(                                                       \
+    const at::Tensor& ARG0,                                                     \
+    const at::Tensor& ARG1,                                                     \
+    const at::Tensor& ARG2                                                      \
+  ) {                                                                           \
+    c10::impl::ExcludeDispatchKeyGuard no_autocast(                             \
+      c10::DispatchKey::Autocast                                                \
+    );                                                                          \
+                                                                                \
+    return c10::Dispatcher::singleton()                                         \
+      .findSchemaOrThrow(                                                       \
+        "torchscience::_" #SCHEMA_NAME,                                         \
+        ""                                                                      \
+      )                                                                         \
+      .typed<at::Tensor(                                                        \
+        const at::Tensor&,                                                      \
+        const at::Tensor&,                                                      \
+        const at::Tensor&                                                       \
+      )>()                                                                      \
+      .call(                                                                    \
+        at::autocast::cached_cast(                                              \
+          at::kFloat,                                                           \
+          ARG0                                                                  \
+        ),                                                                      \
+        at::autocast::cached_cast(                                              \
+          at::kFloat,                                                           \
+          ARG1                                                                  \
+        ),                                                                      \
+        at::autocast::cached_cast(                                              \
+          at::kFloat,                                                           \
+          ARG2                                                                  \
+        )                                                                       \
+      );                                                                        \
+  }
+
+#define TORCHSCIENCE_TERNARY_AUTOCAST_IMPL(SCHEMA_NAME)                         \
+  TORCH_LIBRARY_IMPL(torchscience, Autocast, module) {                          \
+    module.impl(                                                                \
+      "_" #SCHEMA_NAME,                                                         \
+      &SCHEMA_NAME                                                              \
+    );                                                                          \
+  }
+
+#define TORCHSCIENCE_QUATERNARY_AUTOCAST(SCHEMA_NAME, ARG0, ARG1, ARG2, ARG3)   \
+  at::Tensor SCHEMA_NAME(                                                       \
+    const at::Tensor& ARG0,                                                     \
+    const at::Tensor& ARG1,                                                     \
+    const at::Tensor& ARG2,                                                     \
+    const at::Tensor& ARG3                                                      \
+  ) {                                                                           \
+    c10::impl::ExcludeDispatchKeyGuard no_autocast(                             \
+      c10::DispatchKey::Autocast                                                \
+    );                                                                          \
+                                                                                \
+    return c10::Dispatcher::singleton()                                         \
+      .findSchemaOrThrow(                                                       \
+        "torchscience::_" #SCHEMA_NAME,                                         \
+        ""                                                                      \
+      )                                                                         \
+      .typed<at::Tensor(                                                        \
+        const at::Tensor&,                                                      \
+        const at::Tensor&,                                                      \
+        const at::Tensor&,                                                      \
+        const at::Tensor&                                                       \
+      )>()                                                                      \
+      .call(                                                                    \
+        at::autocast::cached_cast(                                              \
+          at::kFloat,                                                           \
+          ARG0                                                                  \
+        ),                                                                      \
+        at::autocast::cached_cast(                                              \
+          at::kFloat,                                                           \
+          ARG1                                                                  \
+        ),                                                                      \
+        at::autocast::cached_cast(                                              \
+          at::kFloat,                                                           \
+          ARG2                                                                  \
+        ),                                                                      \
+        at::autocast::cached_cast(                                              \
+          at::kFloat,                                                           \
+          ARG3                                                                  \
+        )                                                                       \
+      );                                                                        \
+  }
+
+#define TORCHSCIENCE_QUATERNARY_AUTOCAST_IMPL(SCHEMA_NAME)                      \
+  TORCH_LIBRARY_IMPL(torchscience, Autocast, module) {                          \
+    module.impl(                                                                \
+      "_" #SCHEMA_NAME,                                                         \
+      &SCHEMA_NAME                                                              \
+    );                                                                          \
+  }
+
+#define TORCHSCIENCE_QUINARY_AUTOCAST(SCHEMA_NAME, ARG0, ARG1, ARG2, ARG3, ARG4) \
+  at::Tensor SCHEMA_NAME(                                                       \
+    const at::Tensor& ARG0,                                                     \
+    const at::Tensor& ARG1,                                                     \
+    const at::Tensor& ARG2,                                                     \
+    const at::Tensor& ARG3,                                                     \
+    const at::Tensor& ARG4                                                      \
+  ) {                                                                           \
+    c10::impl::ExcludeDispatchKeyGuard no_autocast(                             \
+      c10::DispatchKey::Autocast                                                \
+    );                                                                          \
+                                                                                \
+    return c10::Dispatcher::singleton()                                         \
+      .findSchemaOrThrow(                                                       \
+        "torchscience::_" #SCHEMA_NAME,                                         \
+        ""                                                                      \
+      )                                                                         \
+      .typed<at::Tensor(                                                        \
+        const at::Tensor&,                                                      \
+        const at::Tensor&,                                                      \
+        const at::Tensor&,                                                      \
+        const at::Tensor&,                                                      \
+        const at::Tensor&                                                       \
+      )>()                                                                      \
+      .call(                                                                    \
+        at::autocast::cached_cast(                                              \
+          at::kFloat,                                                           \
+          ARG0                                                                  \
+        ),                                                                      \
+        at::autocast::cached_cast(                                              \
+          at::kFloat,                                                           \
+          ARG1                                                                  \
+        ),                                                                      \
+        at::autocast::cached_cast(                                              \
+          at::kFloat,                                                           \
+          ARG2                                                                  \
+        ),                                                                      \
+        at::autocast::cached_cast(                                              \
+          at::kFloat,                                                           \
+          ARG3                                                                  \
+        ),                                                                      \
+        at::autocast::cached_cast(                                              \
+          at::kFloat,                                                           \
+          ARG4                                                                  \
+        )                                                                       \
+      );                                                                        \
+  }
+
+#define TORCHSCIENCE_QUINARY_AUTOCAST_IMPL(SCHEMA_NAME)                         \
+  TORCH_LIBRARY_IMPL(torchscience, Autocast, module) {                          \
+    module.impl(                                                                \
+      "_" #SCHEMA_NAME,                                                         \
+      &SCHEMA_NAME                                                              \
+    );                                                                          \
+  }
