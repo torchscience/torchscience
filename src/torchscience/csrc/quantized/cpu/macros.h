c879c79a68a8cb72f010993144b8d984772ef0eb
Allen Goodman <allen.goodman@icloud.com>
2025-12-12 13:38:20 -0500

    Add ternary, quaternary, and quinary Quantized CPU kernel macros

--- src/torchscience/csrc/quantized/cpu/macros.h
+++ src/torchscience/csrc/quantized/cpu/macros.h
@@ -90,3 +90,158 @@
         &SCHEMA_NAME##_forward                                                  \
     );                                                                          \
   }
+
+#define TORCHSCIENCE_TERNARY_QUANTIZED_CPU_KERNEL(SCHEMA_NAME, ARG0, ARG1, ARG2) \
+  at::Tensor SCHEMA_NAME##_forward(                                             \
+    const at::Tensor& ARG0,                                                     \
+    const at::Tensor& ARG1,                                                     \
+    const at::Tensor& ARG2                                                      \
+  ) {                                                                           \
+    auto dequantized_##ARG0 = ARG0.dequantize();                                \
+    auto dequantized_##ARG1 = ARG1.dequantize();                                \
+    auto dequantized_##ARG2 = ARG2.dequantize();                                \
+                                                                                \
+    at::Tensor output = at::empty_like(dequantized_##ARG0);                     \
+                                                                                \
+    auto iterator = at::TensorIteratorConfig()                                  \
+        .add_output(output)                                                     \
+        .add_input(dequantized_##ARG0)                                          \
+        .add_input(dequantized_##ARG1)                                          \
+        .add_input(dequantized_##ARG2)                                          \
+        .build();                                                               \
+                                                                                \
+    AT_DISPATCH_FLOATING_TYPES(                                                 \
+        iterator.common_dtype(),                                                \
+        #SCHEMA_NAME,                                                           \
+        [&]() {                                                                 \
+          at::native::cpu_kernel(                                               \
+              iterator,                                                         \
+              [](scalar_t ARG0, scalar_t ARG1, scalar_t ARG2) -> scalar_t {     \
+                return SCHEMA_NAME(ARG0, ARG1, ARG2);                           \
+              }                                                                 \
+          );                                                                    \
+        }                                                                       \
+    );                                                                          \
+                                                                                \
+    return at::quantize_per_tensor(                                             \
+        output,                                                                 \
+        ARG0.q_scale(),                                                         \
+        ARG0.q_zero_point(),                                                    \
+        ARG0.scalar_type()                                                      \
+    );                                                                          \
+  }
+
+#define TORCHSCIENCE_TERNARY_QUANTIZED_CPU_KERNEL_IMPL(SCHEMA_NAME)             \
+  TORCH_LIBRARY_IMPL(torchscience, QuantizedCPU, module) {                      \
+    module.impl(                                                                \
+        "_" #SCHEMA_NAME,                                                       \
+        &SCHEMA_NAME##_forward                                                  \
+    );                                                                          \
+  }
+
+#define TORCHSCIENCE_QUATERNARY_QUANTIZED_CPU_KERNEL(SCHEMA_NAME, ARG0, ARG1, ARG2, ARG3) \
+  at::Tensor SCHEMA_NAME##_forward(                                             \
+    const at::Tensor& ARG0,                                                     \
+    const at::Tensor& ARG1,                                                     \
+    const at::Tensor& ARG2,                                                     \
+    const at::Tensor& ARG3                                                      \
+  ) {                                                                           \
+    auto dequantized_##ARG0 = ARG0.dequantize();                                \
+    auto dequantized_##ARG1 = ARG1.dequantize();                                \
+    auto dequantized_##ARG2 = ARG2.dequantize();                                \
+    auto dequantized_##ARG3 = ARG3.dequantize();                                \
+                                                                                \
+    at::Tensor output = at::empty_like(dequantized_##ARG0);                     \
+                                                                                \
+    auto iterator = at::TensorIteratorConfig()                                  \
+        .add_output(output)                                                     \
+        .add_input(dequantized_##ARG0)                                          \
+        .add_input(dequantized_##ARG1)                                          \
+        .add_input(dequantized_##ARG2)                                          \
+        .add_input(dequantized_##ARG3)                                          \
+        .build();                                                               \
+                                                                                \
+    AT_DISPATCH_FLOATING_TYPES(                                                 \
+        iterator.common_dtype(),                                                \
+        #SCHEMA_NAME,                                                           \
+        [&]() {                                                                 \
+          at::native::cpu_kernel(                                               \
+              iterator,                                                         \
+              [](scalar_t ARG0, scalar_t ARG1, scalar_t ARG2,                   \
+                 scalar_t ARG3) -> scalar_t {                                   \
+                return SCHEMA_NAME(ARG0, ARG1, ARG2, ARG3);                     \
+              }                                                                 \
+          );                                                                    \
+        }                                                                       \
+    );                                                                          \
+                                                                                \
+    return at::quantize_per_tensor(                                             \
+        output,                                                                 \
+        ARG0.q_scale(),                                                         \
+        ARG0.q_zero_point(),                                                    \
+        ARG0.scalar_type()                                                      \
+    );                                                                          \
+  }
+
+#define TORCHSCIENCE_QUATERNARY_QUANTIZED_CPU_KERNEL_IMPL(SCHEMA_NAME)          \
+  TORCH_LIBRARY_IMPL(torchscience, QuantizedCPU, module) {                      \
+    module.impl(                                                                \
+        "_" #SCHEMA_NAME,                                                       \
+        &SCHEMA_NAME##_forward                                                  \
+    );                                                                          \
+  }
+
+#define TORCHSCIENCE_QUINARY_QUANTIZED_CPU_KERNEL(SCHEMA_NAME, ARG0, ARG1, ARG2, ARG3, ARG4) \
+  at::Tensor SCHEMA_NAME##_forward(                                             \
+    const at::Tensor& ARG0,                                                     \
+    const at::Tensor& ARG1,                                                     \
+    const at::Tensor& ARG2,                                                     \
+    const at::Tensor& ARG3,                                                     \
+    const at::Tensor& ARG4                                                      \
+  ) {                                                                           \
+    auto dequantized_##ARG0 = ARG0.dequantize();                                \
+    auto dequantized_##ARG1 = ARG1.dequantize();                                \
+    auto dequantized_##ARG2 = ARG2.dequantize();                                \
+    auto dequantized_##ARG3 = ARG3.dequantize();                                \
+    auto dequantized_##ARG4 = ARG4.dequantize();                                \
+                                                                                \
+    at::Tensor output = at::empty_like(dequantized_##ARG0);                     \
+                                                                                \
+    auto iterator = at::TensorIteratorConfig()                                  \
+        .add_output(output)                                                     \
+        .add_input(dequantized_##ARG0)                                          \
+        .add_input(dequantized_##ARG1)                                          \
+        .add_input(dequantized_##ARG2)                                          \
+        .add_input(dequantized_##ARG3)                                          \
+        .add_input(dequantized_##ARG4)                                          \
+        .build();                                                               \
+                                                                                \
+    AT_DISPATCH_FLOATING_TYPES(                                                 \
+        iterator.common_dtype(),                                                \
+        #SCHEMA_NAME,                                                           \
+        [&]() {                                                                 \
+          at::native::cpu_kernel(                                               \
+              iterator,                                                         \
+              [](scalar_t ARG0, scalar_t ARG1, scalar_t ARG2,                   \
+                 scalar_t ARG3, scalar_t ARG4) -> scalar_t {                    \
+                return SCHEMA_NAME(ARG0, ARG1, ARG2, ARG3, ARG4);               \
+              }                                                                 \
+          );                                                                    \
+        }                                                                       \
+    );                                                                          \
+                                                                                \
+    return at::quantize_per_tensor(                                             \
+        output,                                                                 \
+        ARG0.q_scale(),                                                         \
+        ARG0.q_zero_point(),                                                    \
+        ARG0.scalar_type()                                                      \
+    );                                                                          \
+  }
+
+#define TORCHSCIENCE_QUINARY_QUANTIZED_CPU_KERNEL_IMPL(SCHEMA_NAME)             \
+  TORCH_LIBRARY_IMPL(torchscience, QuantizedCPU, module) {                      \
+    module.impl(                                                                \
+        "_" #SCHEMA_NAME,                                                       \
+        &SCHEMA_NAME##_forward                                                  \
+    );                                                                          \
+  }
