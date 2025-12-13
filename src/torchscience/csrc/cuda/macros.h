20d6153e7504894027296518a7a1b1336136fcf7
Allen Goodman <allen.goodman@icloud.com>
2025-12-13 13:19:10 -0500

    Add TORCHSCIENCE_QUATERNARY_CUDA_KERNEL macro for 4-argument functions

--- src/torchscience/csrc/cuda/macros.h
+++ src/torchscience/csrc/cuda/macros.h
@@ -339,3 +339,150 @@
     );                                                                          \
   }
 
+#define TORCHSCIENCE_QUATERNARY_CUDA_KERNEL(SCHEMA_NAME, ARG0, ARG1, ARG2, ARG3) \
+  at::Tensor SCHEMA_NAME##_forward_kernel(                                      \
+    const at::Tensor& ARG0,                                                     \
+    const at::Tensor& ARG1,                                                     \
+    const at::Tensor& ARG2,                                                     \
+    const at::Tensor& ARG3                                                      \
+  ) {                                                                           \
+    at::Tensor output = at::empty_like(ARG0);                                   \
+                                                                                \
+    auto iterator = at::TensorIteratorConfig()                                  \
+      .add_output(                                                              \
+        output                                                                  \
+      )                                                                         \
+      .add_input(                                                               \
+        ARG0                                                                    \
+      )                                                                         \
+      .add_input(                                                               \
+        ARG1                                                                    \
+      )                                                                         \
+      .add_input(                                                               \
+        ARG2                                                                    \
+      )                                                                         \
+      .add_input(                                                               \
+        ARG3                                                                    \
+      )                                                                         \
+      .build();                                                                 \
+                                                                                \
+    AT_DISPATCH_FLOATING_TYPES(                                                 \
+      iterator.common_dtype(),                                                  \
+      #SCHEMA_NAME,                                                             \
+      [&]() {                                                                   \
+        at::native::gpu_kernel(                                                 \
+          iterator,                                                             \
+          []GPU_LAMBDA(                                                         \
+            scalar_t ARG0,                                                      \
+            scalar_t ARG1,                                                      \
+            scalar_t ARG2,                                                      \
+            scalar_t ARG3                                                       \
+          ) -> scalar_t {                                                       \
+            return SCHEMA_NAME(                                                 \
+              ARG0,                                                             \
+              ARG1,                                                             \
+              ARG2,                                                             \
+              ARG3                                                              \
+            );                                                                  \
+          }                                                                     \
+        );                                                                      \
+      }                                                                         \
+    );                                                                          \
+                                                                                \
+    return output;                                                              \
+  }                                                                             \
+                                                                                \
+  std::tuple<at::Tensor, at::Tensor, at::Tensor, at::Tensor>                    \
+  SCHEMA_NAME##_backward_kernel(                                                \
+    const at::Tensor& gradient_output,                                          \
+    const at::Tensor& ARG0,                                                     \
+    const at::Tensor& ARG1,                                                     \
+    const at::Tensor& ARG2,                                                     \
+    const at::Tensor& ARG3                                                      \
+  ) {                                                                           \
+    at::Tensor grad_##ARG0 = at::empty_like(ARG0);                              \
+    at::Tensor grad_##ARG1 = at::empty_like(ARG1);                              \
+    at::Tensor grad_##ARG2 = at::empty_like(ARG2);                              \
+    at::Tensor grad_##ARG3 = at::empty_like(ARG3);                              \
+                                                                                \
+    auto iterator = at::TensorIteratorConfig()                                  \
+      .add_output(                                                              \
+        grad_##ARG0                                                             \
+      )                                                                         \
+      .add_output(                                                              \
+        grad_##ARG1                                                             \
+      )                                                                         \
+      .add_output(                                                              \
+        grad_##ARG2                                                             \
+      )                                                                         \
+      .add_output(                                                              \
+        grad_##ARG3                                                             \
+      )                                                                         \
+      .add_input(                                                               \
+        gradient_output                                                         \
+      )                                                                         \
+      .add_input(                                                               \
+        ARG0                                                                    \
+      )                                                                         \
+      .add_input(                                                               \
+        ARG1                                                                    \
+      )                                                                         \
+      .add_input(                                                               \
+        ARG2                                                                    \
+      )                                                                         \
+      .add_input(                                                               \
+        ARG3                                                                    \
+      )                                                                         \
+      .build();                                                                 \
+                                                                                \
+    AT_DISPATCH_FLOATING_TYPES(                                                 \
+      iterator.common_dtype(),                                                  \
+      #SCHEMA_NAME "_backward",                                                 \
+      [&]() {                                                                   \
+        at::native::gpu_kernel_multiple_outputs(                                \
+          iterator,                                                             \
+          []GPU_LAMBDA(                                                         \
+            scalar_t gradient,                                                  \
+            scalar_t ARG0,                                                      \
+            scalar_t ARG1,                                                      \
+            scalar_t ARG2,                                                      \
+            scalar_t ARG3                                                       \
+          ) -> std::tuple<scalar_t, scalar_t, scalar_t, scalar_t> {             \
+            auto [grad_##ARG0, grad_##ARG1, grad_##ARG2, grad_##ARG3] =          \
+              SCHEMA_NAME##_backward(                                           \
+                ARG0,                                                           \
+                ARG1,                                                           \
+                ARG2,                                                           \
+                ARG3                                                            \
+              );                                                                \
+            return std::make_tuple(                                             \
+              gradient * grad_##ARG0,                                           \
+              gradient * grad_##ARG1,                                           \
+              gradient * grad_##ARG2,                                           \
+              gradient * grad_##ARG3                                            \
+            );                                                                  \
+          }                                                                     \
+        );                                                                      \
+      }                                                                         \
+    );                                                                          \
+                                                                                \
+    return std::make_tuple(grad_##ARG0, grad_##ARG1, grad_##ARG2, grad_##ARG3); \
+  }
+
+#define TORCHSCIENCE_QUATERNARY_CUDA_KERNEL_IMPL(SCHEMA_NAME)                   \
+  TORCH_LIBRARY_IMPL(                                                           \
+    torchscience,                                                               \
+    CUDA,                                                                       \
+    module                                                                      \
+  ) {                                                                           \
+    module.impl(                                                                \
+      "_" #SCHEMA_NAME,                                                         \
+      &SCHEMA_NAME##_forward_kernel                                             \
+    );                                                                          \
+                                                                                \
+    module.impl(                                                                \
+      "_" #SCHEMA_NAME "_backward",                                             \
+      &SCHEMA_NAME##_backward_kernel                                            \
+    );                                                                          \
+  }
+
