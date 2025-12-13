0bc9eed5e766d346e4d9c9fa531d53647a7c3ee5
Allen Goodman <allen.goodman@icloud.com>
2025-12-13 12:56:01 -0500

    Add CUDA kernel for airy_bi function

---
+++ src/torchscience/csrc/cuda/special_functions/airy_bi.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <torchscience/csrc/impl/special_functions/airy_bi.h>
+#include <torchscience/csrc/cuda/macros.h>
+
+namespace torchscience::cuda::special_functions {
+
+template <typename scalar_t>
+__device__ scalar_t airy_bi(scalar_t x) {
+  return torchscience::impl::special_functions::airy_bi(x);
+}
+
+template <typename scalar_t>
+__device__ scalar_t airy_bi_backward(scalar_t x) {
+  return torchscience::impl::special_functions::airy_bi_backward(x);
+}
+
+TORCHSCIENCE_UNARY_CUDA_KERNEL(airy_bi)
+
+TORCHSCIENCE_UNARY_CUDA_KERNEL_IMPL(airy_bi)
+
+} // namespace torchscience::cuda::special_functions
