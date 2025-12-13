17f2f67cd74ab3230169e195ffd411ebc1a3ca73
Allen Goodman <allen.goodman@icloud.com>
2025-12-13 13:14:47 -0500

    Add CUDA kernel for neville_theta_n function

---
+++ src/torchscience/csrc/cuda/special_functions/neville_theta_n.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <torchscience/csrc/impl/special_functions/neville_theta_n.h>
+#include <torchscience/csrc/cuda/macros.h>
+
+namespace torchscience::cuda::special_functions {
+
+template <typename scalar_t>
+__device__ scalar_t neville_theta_n(scalar_t k, scalar_t u) {
+  return torchscience::impl::special_functions::neville_theta_n(k, u);
+}
+
+template <typename scalar_t>
+__device__ std::tuple<scalar_t, scalar_t> neville_theta_n_backward(scalar_t k, scalar_t u) {
+  auto [grad_k, grad_u] = torchscience::impl::special_functions::neville_theta_n_backward(k, u);
+  return std::make_tuple(grad_k, grad_u);
+}
+
+TORCHSCIENCE_BINARY_CUDA_KERNEL(neville_theta_n, k, u)
+
+TORCHSCIENCE_BINARY_CUDA_KERNEL_IMPL(neville_theta_n)
+
+} // namespace torchscience::cuda::special_functions
