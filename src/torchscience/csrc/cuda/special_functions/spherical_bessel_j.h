bff9b8a48102c7a98b840ea3fdf513e95da5a68a
Allen Goodman <allen.goodman@icloud.com>
2025-12-13 13:15:41 -0500

    Add CUDA kernel for spherical_bessel_j function

---
+++ src/torchscience/csrc/cuda/special_functions/spherical_bessel_j.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <torchscience/csrc/impl/special_functions/spherical_bessel_j.h>
+#include <torchscience/csrc/cuda/macros.h>
+
+namespace torchscience::cuda::special_functions {
+
+template <typename scalar_t>
+__device__ scalar_t spherical_bessel_j(scalar_t n, scalar_t x) {
+  return torchscience::impl::special_functions::spherical_bessel_j(n, x);
+}
+
+template <typename scalar_t>
+__device__ std::tuple<scalar_t, scalar_t> spherical_bessel_j_backward(scalar_t n, scalar_t x) {
+  auto [grad_n, grad_x] = torchscience::impl::special_functions::spherical_bessel_j_backward(n, x);
+  return std::make_tuple(grad_n, grad_x);
+}
+
+TORCHSCIENCE_BINARY_CUDA_KERNEL(spherical_bessel_j, n, x)
+
+TORCHSCIENCE_BINARY_CUDA_KERNEL_IMPL(spherical_bessel_j)
+
+} // namespace torchscience::cuda::special_functions
