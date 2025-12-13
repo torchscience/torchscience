b233c0a377326c9a1092370b7f8530e1ca737cf5
Allen Goodman <allen.goodman@icloud.com>
2025-12-13 13:00:41 -0500

    Add CUDA kernel for bessel_j_derivative function

---
+++ src/torchscience/csrc/cuda/special_functions/bessel_j_derivative.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <torchscience/csrc/impl/special_functions/bessel_j_derivative.h>
+#include <torchscience/csrc/cuda/macros.h>
+
+namespace torchscience::cuda::special_functions {
+
+template <typename scalar_t>
+__device__ scalar_t bessel_j_derivative(scalar_t nu, scalar_t x) {
+  return torchscience::impl::special_functions::bessel_j_derivative(nu, x);
+}
+
+template <typename scalar_t>
+__device__ std::tuple<scalar_t, scalar_t> bessel_j_derivative_backward(scalar_t nu, scalar_t x) {
+  auto [grad_nu, grad_x] = torchscience::impl::special_functions::bessel_j_derivative_backward(nu, x);
+  return std::make_tuple(grad_nu, grad_x);
+}
+
+TORCHSCIENCE_BINARY_CUDA_KERNEL(bessel_j_derivative, nu, x)
+
+TORCHSCIENCE_BINARY_CUDA_KERNEL_IMPL(bessel_j_derivative)
+
+} // namespace torchscience::cuda::special_functions
