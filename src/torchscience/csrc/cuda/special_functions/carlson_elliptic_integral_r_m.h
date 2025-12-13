c327becf5b84d93bf93f5fa5d7491731947a4e5c
Allen Goodman <allen.goodman@icloud.com>
2025-12-13 13:18:03 -0500

    Add CUDA kernel for carlson_elliptic_integral_r_m function

---
+++ src/torchscience/csrc/cuda/special_functions/carlson_elliptic_integral_r_m.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <torchscience/csrc/impl/special_functions/carlson_elliptic_integral_r_m.h>
+#include <torchscience/csrc/cuda/macros.h>
+
+namespace torchscience::cuda::special_functions {
+
+template <typename scalar_t>
+__device__ scalar_t carlson_elliptic_integral_r_m(scalar_t x, scalar_t y, scalar_t z) {
+  return torchscience::impl::special_functions::carlson_elliptic_integral_r_m(x, y, z);
+}
+
+template <typename scalar_t>
+__device__ std::tuple<scalar_t, scalar_t, scalar_t> carlson_elliptic_integral_r_m_backward(scalar_t x, scalar_t y, scalar_t z) {
+  auto [grad_x, grad_y, grad_z] = torchscience::impl::special_functions::carlson_elliptic_integral_r_m_backward(x, y, z);
+  return std::make_tuple(grad_x, grad_y, grad_z);
+}
+
+TORCHSCIENCE_TERNARY_CUDA_KERNEL(carlson_elliptic_integral_r_m, x, y, z)
+
+TORCHSCIENCE_TERNARY_CUDA_KERNEL_IMPL(carlson_elliptic_integral_r_m)
+
+} // namespace torchscience::cuda::special_functions
