ceaee00bf169737a3458e1f7d9df816e2b3fba79
Allen Goodman <allen.goodman@icloud.com>
2025-12-27 10:41:48 -0500

    refactor(cpu,meta): use X-macro for special_functions registration

--- src/torchscience/csrc/meta/special_functions.h
+++ src/torchscience/csrc/meta/special_functions.h
@@ -1,11 +1,10 @@
 #pragma once
 
-#include "operators.h"
+#include "../core/pointwise_registration.h"
+#include "../operators/special_functions.def"
 
-// Template-based registration (Meta operators don't need ImplTraits - shape only)
 TORCH_LIBRARY_IMPL(torchscience, Meta, m_meta_special_functions) {
-    REGISTER_META_UNARY(m_meta_special_functions, gamma);
-    REGISTER_META_BINARY(m_meta_special_functions, chebyshev_polynomial_t);
-    REGISTER_META_TERNARY(m_meta_special_functions, incomplete_beta);
-    REGISTER_META_QUATERNARY(m_meta_special_functions, hypergeometric_2_f_1);
+    #define REGISTER_OP(name, arity, impl) REGISTER_POINTWISE_META(m_meta_special_functions, name, arity);
+    TORCHSCIENCE_SPECIAL_FUNCTIONS(REGISTER_OP)
+    #undef REGISTER_OP
 }
