daec0c04c73231f273f45f8a2bf761f59c98d539
Allen Goodman <allen.goodman@icloud.com>
2025-12-27 10:41:48 -0500

    refactor(cpu,meta): use X-macro for special_functions registration

--- src/torchscience/csrc/cpu/special_functions.h
+++ src/torchscience/csrc/cpu/special_functions.h
@@ -1,21 +1,16 @@
 #pragma once
 
+#include "../core/pointwise_registration.h"
+#include "../operators/special_functions.def"
+
+// Include all impl traits
 #include "../impl/special_functions/gamma_traits.h"
 #include "../impl/special_functions/chebyshev_polynomial_t_traits.h"
 #include "../impl/special_functions/incomplete_beta_traits.h"
 #include "../impl/special_functions/hypergeometric_2_f_1_traits.h"
 
-#include "operators.h"
-
-// Template-based registration using ImplTraits
-using torchscience::impl::special_functions::GammaImpl;
-using torchscience::impl::special_functions::ChebyshevPolynomialTImpl;
-using torchscience::impl::special_functions::IncompleteBetaImpl;
-using torchscience::impl::special_functions::Hypergeometric2F1Impl;
-
 TORCH_LIBRARY_IMPL(torchscience, CPU, m_cpu_special_functions) {
-    REGISTER_CPU_UNARY(m_cpu_special_functions, gamma, GammaImpl);
-    REGISTER_CPU_BINARY(m_cpu_special_functions, chebyshev_polynomial_t, ChebyshevPolynomialTImpl);
-    REGISTER_CPU_TERNARY(m_cpu_special_functions, incomplete_beta, IncompleteBetaImpl);
-    REGISTER_CPU_QUATERNARY(m_cpu_special_functions, hypergeometric_2_f_1, Hypergeometric2F1Impl);
+    #define REGISTER_OP(name, arity, impl) REGISTER_POINTWISE_CPU(m_cpu_special_functions, name, arity, impl);
+    TORCHSCIENCE_SPECIAL_FUNCTIONS(REGISTER_OP)
+    #undef REGISTER_OP
 }
