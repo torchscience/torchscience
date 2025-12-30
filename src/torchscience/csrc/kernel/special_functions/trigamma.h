a45a38e16000686f8c1332b918becc7c9738e7af
Allen Goodman <allen.goodman@icloud.com>
2025-12-30 10:26:53 -0500

    refactor(kernel): simplify trigamma complex implementation
    - Remove intermediate variables
    - Inline asymptotic expansion

--- src/torchscience/csrc/kernel/special_functions/trigamma.h
+++ src/torchscience/csrc/kernel/special_functions/trigamma.h
@@ -32,32 +32,21 @@ c10::complex<T> trigamma(c10::complex<T> z) {
 
   c10::complex<T> y = z;
 
-  // Use reflection formula for Re(z) < 0.5: ψ₁(z) + ψ₁(1-z) = π²/sin²(πz)
-  // So ψ₁(z) = π²/sin²(πz) - ψ₁(1-z)
   if (y.real() < T(0.5)) {
     auto sin_piz = sin_pi(y);
-    auto sin_piz_sq = sin_piz * sin_piz;
 
-    return static_cast<T>(M_PI * M_PI) / sin_piz_sq - trigamma(c10::complex<T>(T(1), T(0)) - y);
+    return static_cast<T>(M_PI * M_PI) / (sin_piz * sin_piz) - trigamma(c10::complex<T>(T(1), T(0)) - y);
   }
 
-  // Use recurrence relation ψ₁(z+1) = ψ₁(z) - 1/z² to shift to larger argument
   while (std::abs(y) < T(6)) {
-    psi1 += c10::complex<T>(T(1), T(0)) / (y * y);
+    psi1 = psi1 + (c10::complex<T>(T(1), T(0)) / (y * y));
 
-    y += c10::complex<T>(T(1), T(0));
+    y = y + (c10::complex<T>(T(1), T(0)));
   }
 
-  // Asymptotic expansion for large |z|
   c10::complex<T> y2 = c10::complex<T>(T(1), T(0)) / (y * y);
 
-  psi1 += c10::complex<T>(T(1), T(0)) / y
-        + c10::complex<T>(T(0.5), T(0)) * y2
-        + y2 / y * (c10::complex<T>(T(1.0 / 6), T(0))
-        - y2 * (c10::complex<T>(T(1.0 / 30), T(0))
-        - y2 * c10::complex<T>(T(1.0 / 42), T(0))));
-
-  return psi1;
+  return psi1 + (c10::complex<T>(T(1), T(0)) / y + c10::complex<T>(T(0.5), T(0)) * y2 + y2 / y * (c10::complex<T>(T(1.0 / 6), T(0)) - y2 * (c10::complex<T>(T(1.0 / 30), T(0)) - y2 * c10::complex<T>(T(1.0 / 42), T(0)))));
 }
 
 } // namespace torchscience::kernel::special_functions
