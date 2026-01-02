0eca5cb04eadcd28558334eaa9004196b30096f2
Allen Goodman <allen.goodman@icloud.com>
2026-01-02 17:34:27 -0500

    feat(sine_wave): implement CPU kernel with tensor parameters

--- src/torchscience/csrc/torchscience.cpp
+++ src/torchscience/csrc/torchscience.cpp
@@ -20,7 +20,8 @@
 #include "cpu/signal_processing/window_functions.h"
 #include "meta/signal_processing/window_functions.h"
 #include "autograd/signal_processing/window_functions.h"
-#include "composite/signal_processing/waveform.h"
+// #include "composite/signal_processing/waveform.h"  // Replaced by CPU kernel
+#include "cpu/signal_processing/waveform/sine_wave.h"
 // noise - CompositeExplicitAutograd
 #include "cpu/signal_processing/noise/white_noise.h"
 #include "cpu/signal_processing/noise/pink_noise.h"
