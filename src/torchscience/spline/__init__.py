dfd42fef9d9c9dd7372225f97dc116dc6ad65f96
Allen Goodman <allen.goodman@icloud.com>
2026-01-02 11:26:08 -0500

    feat(spline): add module structure and exceptions

---
+++ src/torchscience/spline/__init__.py
@@ -0,0 +1,63 @@
+"""Differentiable spline interpolation for PyTorch tensors.
+
+This module provides cubic splines and B-splines with full autograd support.
+
+Cubic Splines
+-------------
+cubic_spline_fit
+    Fit a cubic spline to data points.
+cubic_spline_evaluate
+    Evaluate a cubic spline at query points.
+cubic_spline_derivative
+    Compute derivatives of a cubic spline.
+cubic_spline_integral
+    Compute definite integral of a cubic spline.
+cubic_spline_interpolate
+    Convenience function: fit + evaluate in one call.
+
+B-Splines
+---------
+b_spline
+    Construct a B-spline from knots and control points.
+b_spline_fit
+    Fit a B-spline to data points.
+b_spline_evaluate
+    Evaluate a B-spline at query points.
+b_spline_derivative
+    Compute derivatives of a B-spline.
+b_spline_basis
+    Evaluate B-spline basis functions.
+
+Data Types
+----------
+CubicSpline
+    Piecewise cubic polynomial interpolant.
+BSpline
+    B-spline curve.
+
+Exceptions
+----------
+SplineError
+    Base exception for spline operations.
+ExtrapolationError
+    Query point outside spline domain.
+KnotError
+    Invalid knot vector.
+DegreeError
+    Invalid degree for given knots.
+"""
+
+from torchscience.spline._exceptions import (
+    DegreeError,
+    ExtrapolationError,
+    KnotError,
+    SplineError,
+)
+
+__all__ = [
+    # Exceptions
+    "SplineError",
+    "ExtrapolationError",
+    "KnotError",
+    "DegreeError",
+]
